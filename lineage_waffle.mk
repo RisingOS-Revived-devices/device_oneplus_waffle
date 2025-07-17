cabec38538f438dae2ce88605cf62d8f0f19efd6
flandolf <66240551+flandolf@users.noreply.github.com>
2025-07-17 12:53:30 +1000

    lineage: Update product name and inherit comments in lineage_waffle.mk

--- lineage_waffle.mk
+++ lineage_waffle.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from waffle device
 $(call inherit-product, device/oneplus/waffle/device.mk)
 
-# Inherit some common YAAP stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := yaap_waffle
+PRODUCT_NAME := lineage_waffle
 PRODUCT_DEVICE := waffle
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
